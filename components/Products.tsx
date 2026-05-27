4544162ffcb5edb806c9936451e7f1f3f840d6a9
Danylu8261 <danielassousa29@gmail.com>
2026-05-27 03:20:32 -0300

    Change API endpoint to production server
    Updated API endpoint to use production URL.

--- components/Products.tsx
+++ components/Products.tsx
@@ -35,8 +35,8 @@ export default function Products() {
   const { adicionar } = useCarrinho();
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/produtos")
-      .then((res) => res.json())
+    "https://ultrav-backend.onrender.com/api/produtos"
+    http://localhost:8080/api/produtos
       .then((data) => { setProdutos(data); setLoading(false); })
       .catch(() => { setErro(true); setLoading(false); });
   }, []);
